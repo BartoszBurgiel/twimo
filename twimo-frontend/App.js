563a103decfaccb4ee7ff13f778e609920d8f197
LBeul <l.beul@outlook.de>
2020-01-08 10:56:22 +0100

    Removed corrupted material ui package and swapped it for the official one

--- twimo-frontend/App.js
+++ twimo-frontend/App.js
@@ -1,35 +1,11 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { Toolbar } from "react-native-material-ui";
+import ListOfCards from "./app/components/ListOfCards";
 
-function Greeting(props) {
+export default function Greeting(props) {
   return (
     <View style={{ alignItems: "center" }}>
-      <Text>Hello {props.name}</Text>
-    </View>
-  );
-}
-
-export default function LotsOfGreetings() {
-  return (
-    <View>
-      <Toolbar
-        leftElement="menu"
-        centerElement="Searchable"
-        searchable={{
-          autoFocus: true,
-          placeholder: "Search"
-        }}
-        rightElement={{
-          menu: {
-            icon: "more-vert",
-            labels: ["item 1", "item 2"]
-          }
-        }}
-        onRightElementPress={label => {
-          console.log(label);
-        }}
-      />
+      <ListOfCards />
     </View>
   );
 }
