a5e97ec7dca5b9377910928423b331d2116aad1c
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2020-01-15 23:19:27 +0100

    Initiate verify package and write the verrify password function

---
+++ backend/security/verify.go
@@ -0,0 +1,32 @@
+package security
+
+import (
+	"fmt"
+	"kihmo"
+	"twimo/backend/repository"
+)
+
+// VerifyPassword the user with given Id has put in
+func VerifyPassword(userID, password string, r repository.Repository) (bool, error) {
+
+	// Get user data
+	user, err := r.GetUser(userID)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+
+	// hash password
+	hashedPasswords, err := kihmo.Hash(userID, 60)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+
+	// Compare hashed password and the password in the database
+	if hashedPasswords == user.Password {
+		return true, nil
+	}
+
+	return false, nil
+}
