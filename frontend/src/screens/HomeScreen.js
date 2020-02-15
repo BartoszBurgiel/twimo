614e648aaac5297023dcf613a6bb7dd1f6a1ddaf
LBeul <l.beul@outlook.de>
2020-02-15 02:09:00 +0100

    Restructure homeScreen
    - Add inputFields
    - Add touchableOpacity
    - Fake Login Functionality
    - Add TODO for later signupPage integration

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,39 @@
 // Import core functionalities from react & react-native library
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 // Import color utility for consistent styling
 import color from "../../utils/colorPallet";
+
+//TODO: Implement real credential check!
 
 // Define HomeScreen
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>twimo</Text>
-      <Button
-        title="Locations"
+      <Text style={styles.header}>twimo</Text>
+      <TextInput style={styles.input} autoCompleteType="off" />
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        autoCompleteType="off"
+      />
+      <TouchableOpacity
+        style={styles.buttonPrimary}
         onPress={() => navigation.navigate("List")}
+      >
+        <Text style={styles.buttonPrimaryText}>login</Text>
+      </TouchableOpacity>
+      <Button
+        title="Sign up"
+        onPress={() => console.log("Sir, we need a fucking signup page.")}
       ></Button>
     </View>
   );
@@ -26,10 +47,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  textStyle: {
+  header: {
     fontSize: 50,
     fontWeight: "bold",
     color: color.brand.normal
+  },
+  input: {
+    height: 50,
+    width: "70%",
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 12,
+    backgroundColor: color.brand.light,
+    color: color.brand.dark
+  },
+  buttonPrimary: {
+    backgroundColor: color.brand.normal,
+    height: 50,
+    width: "35%",
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  buttonPrimaryText: {
+    color: color.white,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    fontSize: 20
   }
 });
 
