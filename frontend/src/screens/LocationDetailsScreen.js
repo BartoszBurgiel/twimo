e0fc49f06e146c19b68d76d5dffb595a75f89703
LBeul <l.beul@outlook.de>
2020-03-03 17:08:44 +0100

    Implement fetching method for reviwe data
    - Delete dummyData array
    - Enable fetch() for working with APIs
    - Create reviews.json to be hosted with live-server

--- frontend/src/screens/LocationDetailsScreen.js
+++ frontend/src/screens/LocationDetailsScreen.js
@@ -1,5 +1,5 @@
 // Import core functionality dependencies from react & react-native
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   ScrollView,
@@ -17,48 +17,20 @@ import RatingStars from "../components/RatingStars";
 import FeatureIcons from "../components/FeatureIcons";
 import ReviewCard from "../components/ReviewCard";
 
-// Define some dummy Review data
-const dummyReviews = [
-  {
-    key: "1",
-    name: "Ford Prefect",
-    rating: 5,
-    avatar: "https://api.adorable.io/avatars/50/fordprefect.png",
-    content:
-      "Klasse Handwerksbäcker, gemütliches und stilvolles Café mit hervorragenden Backwaren und bestem Kaffee. Unbedingt den Cafe Latte probieren - ein echter Genuss an diesen kalten Winterertagen!"
-  },
-  {
-    key: "2",
-    name: "Zaphod Beeblebrox",
-    rating: 4,
-    avatar: "https://api.adorable.io/avatars/50/zaphodbeeblebrox.png",
-    content:
-      "Klasse Cafe, gemütliches und stilvolles Ambiente mit hervorragenden Backwaren und gutem Kaffee. Unbedingt den Cafe Latte probieren - ein echter Genuss an diesen kalten Winterertagen!"
-  },
-  {
-    key: "3",
-    name: "Arthur Weasley",
-    rating: 2,
-    avatar: "https://api.adorable.io/avatars/50/arthurweasley.png",
-    content:
-      "Naja, gemütliches isses schon irgendwie. Aber der Durchzug ist heftig. Unbedingt ne Jacke mitnehmen - sonst gibt's ne fette Erkältung! Ansonsten kann man da schon hingehen..."
-  },
-  {
-    key: "4",
-    name: "Bellatrix Lestrange",
-    rating: 1,
-    avatar: "https://api.adorable.io/avatars/50/bellatrixlestrange.png",
-    content:
-      "Also ich find des nicht gut. Die Backwaren sind ganz ok, aber wenn'de kein Englisch kannst, bist in dem Laden uffgeschmissen. Da bleib ich beim Bäcker im Dorf!"
-  }
-];
-
 // Define LocationDetailsScreen
 const LocationDetailsScreen = props => {
   //FIXME: Virtualized Lists Warning
 
+  // Set up component's state
+  const [reviews, setReviews] = useState(0);
+
   // Grab locationData from the props and store it in data const
   const data = props.navigation.state.params.locationData;
+
+  // Fetch review data from API
+  fetch("http://127.0.0.1:5500/frontend/data/reviews.json")
+    .then(response => response.json())
+    .then(data => setReviews(data));
 
   return (
     <ScrollView style={styles.container}>
@@ -79,7 +51,7 @@ const LocationDetailsScreen = props => {
         <Text style={styles.secondaryHeaderText}>Kommentare</Text>
       </View>
       <FlatList
-        data={dummyReviews}
+        data={reviews}
         style={styles.contentWrapper}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => <ReviewCard reviewData={item} />}
