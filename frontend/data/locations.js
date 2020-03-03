865e966a59a202e1a15fb216ac2fe04342644f04
LBeul <l.beul@outlook.de>
2020-03-03 01:02:40 +0100

    Outsourcing dummy data

--- frontend/data/locations.js
+++ frontend/data/locations.js
@@ -1,14 +1,3 @@
-// Import core functionalities from react & react-native library
-import React from "react";
-import { View, FlatList, StyleSheet } from "react-native";
-
-// Import custom components
-import LocationCard from "../components/LocationCard";
-
-// Import color utility for consistent styling
-import color from "../../utils/colorPallet";
-
-// Define some dummy data
 const dummyData = [
   {
     key: "1",
@@ -90,37 +79,3 @@ const dummyData = [
       "https://www.coffee-fellows.com/wp-content/uploads/2017/03/coffee-fellows_cf_rs_breisgau_0017.jpg"
   }
 ];
-
-// Define the ListScreen
-const ListScreen = ({ navigation }) => {
-  fetch("../../data/locations.js").then(response => {
-    console.log(response);
-  });
-
-  // Iterate over the given nodes and display them as LocationCards in a FlatList
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={dummyData}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <LocationCard data={item} navigation={navigation} />
-        )}
-      />
-    </View>
-  );
-};
-
-// Define the styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: color.white,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
-
-// Export the above defined screen
-export default ListScreen;
